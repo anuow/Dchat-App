5092c6a83a421f8eb3821b84cfcda5fab9804077
anu <anirudh157290@gmail.com>
2025-12-10 20:18:41 +0530

    Update user.js

--- src/user.js
+++ src/user.js
@@ -8,3 +8,21 @@ export function recallSession() {
     user.recall({sessionStorage: true });
     user.get('alias').once(a=> alias.set(a || ''));
 }
+
+export function signup(name, pass) {
+    return new Promise((res, rej) => {
+        user.create(name, pass, ack => {
+            if ('err' in ack) return rej(ack.err);
+            user.auth(name, pass, a => {
+                if ('err' in a) return rej(a.err);
+                user.get('profile').put({ alias: name });
+                alias.set(name);
+                res(a);
+            })
+        })
+    })
+}
+    export function logout() {
+  user.leave();
+  alias.set('');
+    }
